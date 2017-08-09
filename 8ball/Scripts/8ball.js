49833516d3ec02a3ed620f68e1e07f18b2990398
drobin45 <drobin45@kent.edu>
2017-08-09 11:01:48 -0400

    Added styling, made 8ball image clickable

--- 8ball/Scripts/8ball.js
+++ 8ball/Scripts/8ball.js
@@ -1,5 +1,5 @@
 ﻿function getPhrase() {
-    var phrases = ['Maybe', 'Absolutely', 'Not A Chance', 'Outlook Good', 'Try Again', 'It Is Certain', 'Better Not Tell You Now', 'Dont Count on It', 'Very doubtful'];
+    var phrases = ['Maybe', 'Absolutely', 'Not A Chance', 'Outlook Good', 'Try Again', 'It Is Certain', 'No!', 'Yes!', 'Very doubtful'];
 
     var num = Math.floor(Math.random() * phrases.length);
     document.getElementById("fortune").innerHTML = phrases[num];    
